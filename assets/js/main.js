11f97c06adbf57b946fefbd90b133702067ab7cb
Craig Da Silva <craig_344@hotmail.com>
2019-03-19 11:28:52 +0530

    basic ,board

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
 var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#808080";
-ctx.fillRect(0,0,100,100);
+for(i=0; i<500; i+=50){
+    for(j=0; j<500; j+=50){
+        ctx.fillRect(i,j,49,49);
+    }
+}
