00cf8d25019f348bfc2b9ad3d31a493bf94a3e84
Craig Da Silva <craig_344@hotmail.com>
2019-03-20 16:42:20 +0530

    detects clicks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,32 @@
 var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext("2d");
+var squares = [[]];
 ctx.fillStyle = "#808080";
 for(i=0; i<500; i+=50){
     for(j=0; j<500; j+=50){
         ctx.fillRect(i,j,49,49);
     }
 }
+
+canvas.addEventListener('click', (e) => {
+    ctx.fillStyle = "rgba(200,0,0,1)";
+    var x = e.clientX-canvas.offsetLeft
+    ,y = e.clientY-canvas.offsetTop;
+    
+    x-= x%50;
+    y-=y%50;
+    
+    ctx.fillRect(x,y,49,49); 
+ });
+
+ canvas.addEventListener('contextmenu', (e) => {
+    ctx.fillStyle = "#808080";
+     e.preventDefault();
+    var x = e.clientX-canvas.offsetLeft
+    ,y = e.clientY-canvas.offsetTop;
+    
+    x-= x%50;
+    y-=y%50;
+    
+    ctx.fillRect(x,y,49,49); 
+ });
