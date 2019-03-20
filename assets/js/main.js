2474d3f0ba3ec187423af3858a52f0e8ec3e4c45
Craig Da Silva <craig_344@hotmail.com>
2019-03-20 17:11:41 +0530

    added 2d array and display which sqiare is clicked

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext("2d");
-var squares = [[]];
+var squares = [];
 ctx.fillStyle = "#808080";
+for(i=0; i<10; i++){
+    squares[i] = [];
+}
+
 for(i=0; i<500; i+=50){
     for(j=0; j<500; j+=50){
         ctx.fillRect(i,j,49,49);
+        squares[i/50][j/50] = 0;
     }
 }
 
