854147b2ac63e4c2d44dfe95431c3c422acb4c61
Craig Da Silva <craig_344@hotmail.com>
2019-03-20 17:18:02 +0530

    perform action according to current state

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,40 +2,43 @@ var canvas = document.getElementById("mainCanvas");
 var ctx = canvas.getContext("2d");
 var squares = [];
 ctx.fillStyle = "#808080";
-for(i=0; i<10; i++){
+for (i = 0; i < 10; i++) {
     squares[i] = [];
 }
 
-for(i=0; i<500; i+=50){
-    for(j=0; j<500; j+=50){
-        ctx.fillRect(i,j,49,49);
-        squares[i/50][j/50] = 0;
+for (i = 0; i < 500; i += 50) {
+    for (j = 0; j < 500; j += 50) {
+        ctx.fillRect(i, j, 49, 49);
+        squares[i / 50][j / 50] = 0;
     }
 }
 
 canvas.addEventListener('click', (e) => {
     ctx.fillStyle = "rgba(200,0,0,1)";
-    var x = e.clientX-canvas.offsetLeft
-    ,y = e.clientY-canvas.offsetTop;
-    
-    x-= x%50;
-    y-=y%50;
-    
-    ctx.fillRect(x,y,49,49);
-    squares[x/50][y/50] = 1; 
-    console.log(squares[x/50][y/50]);
- });
+    var x = e.clientX - canvas.offsetLeft
+        , y = e.clientY - canvas.offsetTop;
 
- canvas.addEventListener('contextmenu', (e) => {
+    x -= x % 50;
+    y -= y % 50;
+    if (squares[x / 50][y / 50] == 0) {
+        ctx.fillRect(x, y, 49, 49);
+        squares[x / 50][y / 50] = 1;
+        console.log(squares[x / 50][y / 50]);
+    }
+});
+
+canvas.addEventListener('contextmenu', (e) => {
     ctx.fillStyle = "#808080";
-     e.preventDefault();
-    var x = e.clientX-canvas.offsetLeft
-    ,y = e.clientY-canvas.offsetTop;
-    
-    x-= x%50;
-    y-=y%50;
-    
-    ctx.fillRect(x,y,49,49); 
-    squares[x/50][y/50] = 0; 
-    console.log(squares[x/50][y/50]);
- });
+    e.preventDefault();
+    var x = e.clientX - canvas.offsetLeft
+        , y = e.clientY - canvas.offsetTop;
+
+    x -= x % 50;
+    y -= y % 50;
+
+    if (squares[x / 50][y / 50] == 1) {
+        ctx.fillRect(x, y, 49, 49);
+        squares[x / 50][y / 50] = 0;
+        console.log(squares[x / 50][y / 50]);
+    }
+});
