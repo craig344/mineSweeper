3c58753a5ae63a8beda18b664d5230fa6378cfb6
Craig Da Silva <craigdasilva@localhost.localdomain>
2019-03-26 12:10:13 +0530

    oop basic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,41 @@
-function click(ctx, colour, x, y, grid, click) {
+function box() {
+    this.flag = "no";
+    this.click = "no";
+    this.bomb = "no";
+    this.red = "no";
+}
+
+function click(ctx, x, y, grid, click) {
     x -= x % 50;
     y -= y % 50;
     var nx = x / 50,
         ny = y / 50;
 
-    if (grid[nx][ny] != click) {
-        ctx.fillStyle = colour;
-        ctx.fillRect(x, y, 49, 49);
-        grid[nx][ny] = click;
-        console.log(grid[nx][ny], nx, ny);
+    if(click == "right" && grid[nx][ny].red == "no"){
+        grid = fillColour(grid, ctx, x, y, "rgba(200,0,0,1)");
+        grid[nx][ny].red = "yes";
+    }else if(click == "left" && grid[nx][ny].red == "yes"){
+        grid = fillColour(grid, ctx, x, y, "#808080");
+        grid[nx][ny].red = "no";
     }
+    
     return grid;
 }
 
-function fillCanvas(ctx, grid) {
-    ctx.fillStyle = "#808080";
-    for (i = 0; i < 10; i++) {
-        grid[i] = [];
-    }
+function fillColour(grid, ctx, x, y, colour){
+    ctx.fillStyle = colour;
+    ctx.fillRect(x, y, 49, 49);
+    return grid;
+}
 
-    for (i = 0; i < 500; i += 50) {
-        for (j = 0; j < 500; j += 50) {
+function fillCanvas(ctx, size) {
+    ctx.fillStyle = "#808080";
+
+    for (i = 0; i < 50*size; i += 50) {
+        for (j = 0; j < 50*size; j += 50) {
             ctx.fillRect(i, j, 49, 49);
-            grid[i / 50][j / 50] = 0;
         }
     }
-    return grid;
 }
 
 function placeBombs(grid){
@@ -33,20 +43,26 @@ function placeBombs(grid){
     for(i = 0; j<10; i++){
         bx = Math.floor((Math.random() * 10));
         by = Math.floor((Math.random() * 10));
-        if(grid[bx][by] == 2){
+        if(grid[bx][by].bomb == "yes"){
             i--;
         }else{
-            grid[bx][by] = 2;
+            grid[bx][by] = "yes";
         }
     }
     return grid;
-
 }
 
-function addBoxes(grid, box){
-    for(i = 0; i < 10; i++){
-        for(j = 0; j < 10; j++){
-            grid[i][j] = new box;
+function make2d(grid, size){
+    for (i = 0; i < size; i++) {
+        grid[i] = [];
+    }
+    return grid
+}
+
+function addBoxes(grid, size){
+    for(i = 0; i < size; i++){
+        for(j = 0; j < size; j++){
+            grid[i][j] = new box();
         }
     }
     return grid;
@@ -56,22 +72,19 @@ function init() {
     var canvas = document.getElementById("mainCanvas");
     var ctx = canvas.getContext("2d");
     var grid = [];
-    var box = {
-        flag: "no",
-        click: "no",
-        bomb: "no"
-    };
+    var size = 10;
 
-    grid = fillCanvas(ctx, grid);
-    grid = addBoxes(grid, box);
+    grid = make2d(grid, size);
+    fillCanvas(ctx, size);
+    grid = addBoxes(grid, size);
 
     canvas.addEventListener('click', (e) => {
-        grid = click(ctx, "rgba(200,0,0,1)", e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, grid, 1);
+        grid = click(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, grid, "left");
     });
 
     canvas.addEventListener('contextmenu', (e) => {
         e.preventDefault();
-        grid = click(ctx, "#808080", e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, grid, 0);
+        grid = click(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, grid, "right");
     });
 
 }
