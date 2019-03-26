261cae9917985ec20e8a17c88e9deec763145b1b
Craig Da Silva <craigdasilva@localhost.localdomain>
2019-03-26 11:25:46 +0530

    implementing oo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,77 @@
-function click(ctx, colour, x, y, squares, click) {
+function click(ctx, colour, x, y, grid, click) {
     x -= x % 50;
     y -= y % 50;
     var nx = x / 50,
         ny = y / 50;
 
-    if (squares[nx][ny] != click) {
+    if (grid[nx][ny] != click) {
         ctx.fillStyle = colour;
         ctx.fillRect(x, y, 49, 49);
-        squares[nx][ny] = click;
-        console.log(squares[nx][ny], nx, ny);
+        grid[nx][ny] = click;
+        console.log(grid[nx][ny], nx, ny);
     }
-    return squares;
+    return grid;
 }
 
-function fillCanvas(ctx, squares) {
+function fillCanvas(ctx, grid) {
     ctx.fillStyle = "#808080";
     for (i = 0; i < 10; i++) {
-        squares[i] = [];
+        grid[i] = [];
     }
 
     for (i = 0; i < 500; i += 50) {
         for (j = 0; j < 500; j += 50) {
             ctx.fillRect(i, j, 49, 49);
-            squares[i / 50][j / 50] = 0;
+            grid[i / 50][j / 50] = 0;
         }
     }
-    return squares;
+    return grid;
 }
 
-function placeBombs(squares){
+function placeBombs(grid){
     var bx, by;
     for(i = 0; j<10; i++){
         bx = Math.floor((Math.random() * 10));
         by = Math.floor((Math.random() * 10));
-        if(squares[bx][by] == 2){
+        if(grid[bx][by] == 2){
             i--;
         }else{
-            squares[bx][by] = 2;
+            grid[bx][by] = 2;
         }
     }
-    return squares;
+    return grid;
 
+}
+
+function addBoxes(grid, box){
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 10; j++){
+            grid[i][j] = new box;
+        }
+    }
+    return grid;
 }
 
 function init() {
     var canvas = document.getElementById("mainCanvas");
     var ctx = canvas.getContext("2d");
-    var squares = [];
+    var grid = [];
+    var box = {
+        flag: "no",
+        click: "no",
+        bomb: "no"
+    };
 
-    squares = fillCanvas(ctx, squares);
+    grid = fillCanvas(ctx, grid);
+    grid = addBoxes(grid, box);
 
     canvas.addEventListener('click', (e) => {
-        squares = click(ctx, "rgba(200,0,0,1)", e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, squares, 1);
+        grid = click(ctx, "rgba(200,0,0,1)", e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, grid, 1);
     });
 
     canvas.addEventListener('contextmenu', (e) => {
         e.preventDefault();
-        squares = click(ctx, "#808080", e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, squares, 0);
+        grid = click(ctx, "#808080", e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop, grid, 0);
     });
 
 }
